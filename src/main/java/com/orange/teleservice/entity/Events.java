b65eec1c64e64a87e2525d712a3c4bd5dd699982
Raghavendra.A <User1@LP-5CD9176M5C.HCLT.CORP.HCL.IN>
2020-02-17 13:39:12 +0530

    Adding setters and getters for events.

--- src/main/java/com/orange/teleservice/entity/Events.java
+++ src/main/java/com/orange/teleservice/entity/Events.java
@@ -1,9 +1,20 @@
 package com.orange.teleservice.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Events {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
